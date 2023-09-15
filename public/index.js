a8e34bca5a85ef15fd6de98992952ef0eccc67d5
bendragon2700 <143348750+bendragon2700@users.noreply.github.com>
2023-09-15 13:31:01 +0300

    Update index.js
    cookies include

--- public/index.js
+++ public/index.js
@@ -1,28 +1,28 @@
 const nameInput = document.getElementById('name');
-			const passInput = document.getElementById('password');
-			const submitbutton = document.getElementById('submitbutton'); // Rename the button
+const passInput = document.getElementById('password');
+const submitbutton = document.getElementById('submitbutton'); // Rename the button
+//const resultSpan = document.getElementById('result');
+		
+submitbutton.addEventListener('click', async () => {
+	const uname = nameInput.value;
+	const upass = passInput.value;
 			
-			//const resultSpan = document.getElementById('result');
-		
-			submitbutton.addEventListener('click', async () => {
-			  const uname = nameInput.value;
-			  const upass = passInput.value;
-		
-			  const response = await fetch('/login', {
-				method: 'POST',
-				headers: {
-				  'Content-Type': 'application/json'
-				},
-				body: JSON.stringify({ uname, upass })
-			  });
-		
-			  const data = await response.json();
-			  
-			  if (data === 1) {
-				// Successful login, redirect to todolist.html
-				window.location.href = '/todolist.html';
-			  } else {
-				// Failed login, show an error message
-				window.location.href = '/index.html';
-			  }
-		});
+	const response = await fetch('/login', {
+	method: 'POST',
+	headers: {
+	'Content-Type': 'application/json'
+	},
+		cookie: document.cookie,
+		body: JSON.stringify({ uname, upass })
+	});
+			
+	const data = await response.json();
+				
+	if (data === 1) {
+		// Successful login, redirect to todolist.html
+		window.location.href = '/todolist.html';
+	} else {
+		// Failed login, show an error message
+		window.location.href = '/index.html';
+	}
+});
